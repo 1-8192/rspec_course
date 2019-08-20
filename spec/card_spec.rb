b3abadd4f5b4d4aa1086e77fef56c34ffabf5a5b
1-8192 <allegranzia@gmail.com>
2019-08-20 17:50:54 -0400

    added 2 tests for card value and suit

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,24 @@
 
 class Card 
 
-    attr_reader :value
+    attr_reader :value, :suit
 
-    def initialize(value)
+    def initialize(value, suit)
         @value = value
+        @suit = suit
     end
 
 end 
 
-RSpec.describe 'Card' do 
+RSpec.describe Card do 
     it 'has a value' do 
-        card = Card.new('Ace of Spades')
-        expect(card.value).to eq('Ace of Spades')
+        card = Card.new('Ace', 'Spades')
+        expect(card.value).to eq('Ace')
+    end
+
+    it 'has a suit' do
+        card = Card.new('Ace', 'Spades')
+        expect(card.suit).to eq('Spades')
     end
 end
 
