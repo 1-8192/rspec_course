130be58f4d036223d4d4fe6c52657cc093653f38
1-8192 <allegranzia@gmail.com>
2019-08-20 17:29:52 -0400

    got first test to pass

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,18 @@
+
+class Card 
+
+    attr_reader :value
+
+    def initialize(value)
+        @value = value
+    end
+
+end 
+
 RSpec.describe 'Card' do 
     it 'has a value' do 
         card = Card.new('Ace of Spades')
         expect(card.value).to eq('Ace of Spades')
     end
 end
+
