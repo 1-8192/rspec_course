2c61c897b51970fc52b09b04797f70340ea7a733
1-8192 <allegranzia@gmail.com>
2019-08-21 15:09:04 -0400

    practiced explicit subject

--- spec/implicit_subject_spec.rb
+++ spec/implicit_subject_spec.rb
@@ -1,8 +1,12 @@
-RSpec.describe Hash do 
-    let(:subject) { Hash.new }
+RSpec.describe Array do 
 
-    it 'should start empyt' do
+    it "has initial length of 0" do 
         expect(subject.length).to eq(0)
-    end 
-    
+    end
+
+    it "has more items after push" do 
+        subject.push('test')
+        expect(subject.length).to eq(1)
+    end
+
 end
