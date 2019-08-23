a0542a8a06a1ba6dd23852baea30d6e02270e8fb
1-8192 <allegranzia@gmail.com>
2019-08-23 09:47:02 -0400

    equal matcher

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe 'equality matchers' do
             expect(a).not_to eql(b)
         end 
     end 
+
+    describe 'equal and be matchers' do 
+        let(:c) {  [1, 2, 3] }
+        let(:d) { [1,2,3] }
+        let(:e) { c }
+        
+        it 'cares about object identity' do 
+            expect(c).to eq(d)
+            expect(c).to equal(d)
+            expect(c).to equal(e)
+        end
+    end
 end 
