7c8eddf0e104ce2af1fcb669214a55c4dffe75a8
1-8192 <allegranzia@gmail.com>
2019-08-26 10:10:36 -0400

    counts

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -35,6 +35,7 @@ class Movie
             @actor.act 
             @actor.fall_off_ladder
             @actor.light_on_fire
+            @actor.act
         end 
     end 
 end 
@@ -45,10 +46,11 @@ RSpec.describe Movie do
 
     describe '#start_shooting method' do
        it 'expects to do 3 actions' do 
-            expect(stuntman).to receive(:ready?)
-            expect(stuntman).to receive(:act)
-            expect(stuntman).to receive(:fall_off_ladder)
-            expect(stuntman).to receive(:light_on_fire)
+            # expect(stuntman).to receive(:light_on_fire).once
+            # expect(stuntman).to receive(:light_on_fire).exactly(1).times
+            expect(stuntman).to receive(:light_on_fire).at_most(4).times
+            expect(stuntman).to receive(:act).exactly(2).times
+
             subject.start_shooting
        end
     end   
